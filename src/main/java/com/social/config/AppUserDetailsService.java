2501018bc28256577e8ce80a13b963e8e3435c81
kamal <kamal.berriga@gmail.com>
2017-11-10 16:05:17 +0000

    adding user details service

---
+++ src/main/java/com/social/config/AppUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.social.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.social.entities.User;
+import com.social.services.UserService;
+
+
+/**
+ * This Service class for providing the user credentials from the database.
+ * 
+ * @author kamal berriga
+ *
+ */
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	UserService userService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userService.find(username);
+		return  user;
+	}
+
+}
