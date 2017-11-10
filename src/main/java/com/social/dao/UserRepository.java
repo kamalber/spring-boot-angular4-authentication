43d006fbb97c6cd7284a591721c6257e41b4b7bd
kamal <kamal.berriga@gmail.com>
2017-11-10 14:12:25 +0000

    creating the user repository

---
+++ src/main/java/com/social/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package com.social.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.social.entities.User;
+
+/* this the user  Repository interface  */ 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+public User findOneByUsername(String username);
+}
