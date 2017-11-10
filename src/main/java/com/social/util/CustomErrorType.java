99cc2eb724a0f3d0b14d9060d1dcff16c36ea716
kamal <kamal.berriga@gmail.com>
2017-11-10 16:07:11 +0000

    creating a custom error class to handle message error type

---
+++ src/main/java/com/social/util/CustomErrorType.java
@@ -0,0 +1,15 @@
+package com.social.util;
+
+public class CustomErrorType {
+
+    private String errorMessage;
+
+    public CustomErrorType(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+}
