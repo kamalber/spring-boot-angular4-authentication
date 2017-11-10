be922b95122435287162f08657bde1e86935fd28
kamal <kamal.berriga@gmail.com>
2017-11-10 13:18:29 +0000

    project creation/configuration

---
+++ src/main/java/com/social/SpringBootSocialAuthApplication.java
@@ -0,0 +1,12 @@
+package com.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocialAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSocialAuthApplication.class, args);
+	}
+}
