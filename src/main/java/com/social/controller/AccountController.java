9f5e5c92c29353ea935a52d015bd04545b2b2693
kamal <kamal.berriga@gmail.com>
2017-11-10 16:06:22 +0000

    creating the the rest api controller

---
+++ src/main/java/com/social/controller/AccountController.java
@@ -0,0 +1,12 @@
+package com.social.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("account")
+public class AccountController {
+
+
+
+}
