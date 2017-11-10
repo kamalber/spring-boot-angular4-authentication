2386561a118294cac97c34a6442177c61c047dc6
kamal <kamal.berriga@gmail.com>
2017-11-10 15:50:46 +0000

    adding confi annoncation to allow the test

--- src/test/java/com/social/service/UserServiceTest.java
+++ src/test/java/com/social/service/UserServiceTest.java
@@ -2,16 +2,24 @@ package com.social.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.social.entities.User;
 import com.social.services.UserService;
 
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
 public class UserServiceTest {
 
 	@Autowired
@@ -23,7 +31,7 @@ public class UserServiceTest {
 	public void testSave() {
 
 		User user = new User();
-		user.setUsername("user.user@user.com");
+		user.setUsername("user.user");
 		user.setFullName("kamalbberriga");
 		entityManager.persist(user);
 		entityManager.flush();
