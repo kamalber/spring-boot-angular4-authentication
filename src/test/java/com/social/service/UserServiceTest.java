e883e036a9be086aa0766d86dfbe2074812151dc
kamal <kamal.berriga@gmail.com>
2017-11-10 15:28:20 +0000

    creating the userService class test

---
+++ src/test/java/com/social/service/UserServiceTest.java
@@ -0,0 +1,35 @@
+package com.social.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserServiceTest {
+
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFindString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
