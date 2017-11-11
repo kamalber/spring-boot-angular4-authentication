e982c8dc1ffe5d86db9e9dfba585f1868ccd9ca4
kamal <kamal.berriga@gmail.com>
2017-11-11 23:46:48 +0000

    login component content

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {User} from "../../model/model.user";
+import {AuthService} from "../../services/auth.service";
+
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+  user: User=new User();
+  constructor(public authService :AuthService) { }
 
-  constructor() { }
+
 
   ngOnInit() {
   }
 
+  login(){
+    this.authService.logIn(this.user)
+      .subscribe(data=>{
+          console.log("is user connected : "+data.authenticated, " username :  "+data.principal.username);
+        },err=>{
+          console.log("login error");
+        }
+      )
+  }
 }
