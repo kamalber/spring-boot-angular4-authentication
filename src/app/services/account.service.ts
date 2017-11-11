cd0c35d4217757bf98bcbfc1117bc6a2810a09b7
kamal <kamal.berriga@gmail.com>
2017-11-11 23:48:34 +0000

    creating account service with createAccount methode

---
+++ src/app/services/account.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {User} from "../model/model.user";
+import {Http} from "@angular/http";
+
+@Injectable()
+export class AccountService {
+
+  constructor(public http: Http) { }
+
+  createAccount(user:User){
+    console.log(user.username);
+    return this.http.post('http://localhost:8080/account/register',user)
+      .map(resp=>resp.json());
+  }
+}
