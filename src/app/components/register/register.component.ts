9b4494b5cd4caa4b6c18ae14f1797855eb4b8258
kamal <kamal.berriga@gmail.com>
2017-11-11 13:49:13 +0000

    creating the register component

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
