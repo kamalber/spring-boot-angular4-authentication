1d24aa8e6ba1a4ad9327c19817eac66769a42ea9
kamal <kamal.berriga@gmail.com>
2017-11-13 04:42:15 +0000

    facebook login service

--- src/app/services/facebook-custom.service.ts
+++ src/app/services/facebook-custom.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import {FacebookService, InitParams} from "ngx-facebook";
+import {FacebookService, InitParams, LoginResponse} from "ngx-facebook";
+import {FacebookUser} from "../model/mode.FacebookUser";
 
 @Injectable()
 export class FacebookCustomService {
+  public user: FacebookUser =new FacebookUser();
+  constructor(private fb: FacebookService, ) {
 
-  constructor(private fb: FacebookService) {
-
+    // this will init the app using the facebook app id
     let initParams: InitParams = {
       appId: '1900664226929744',
       xfbml: true,
@@ -13,6 +15,26 @@ export class FacebookCustomService {
     };
     this.fb.init(initParams);
   }
+login(){
+  this.fb.login()
+ .then((response: LoginResponse) =>{
+   //getting some user info , the user_id is the most importent
+  this.fb.api('/me?fields=name,email,picture,first_name,last_name').then((response) => {
 
+    this.user.id = response.id;
+    this.user.fullName = response.firstName+ " "+response.lastName;
+    this.user.photoUrl = "https://graph.facebook.com/" + response.id + "/picture?type=normal";
+    localStorage.setItem("currentUserFB",JSON.stringify(this.user));
+    console.log("uuu"+ this.user.id);
+  }).catch((error: any) => console.error(error));
+  }).catch((error: any) => console.error(error));
+
+}
+  getUserAlbums(){
+    console.log("uuu"+ this.user.id);
+    this.fb.api('/'+this.user.id+'/albums').then((response) => {
+console.log(response);
+    }).catch((error: any) => console.error(error));
+  }
 
 }
