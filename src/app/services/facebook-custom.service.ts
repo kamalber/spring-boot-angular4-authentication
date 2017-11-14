7db90068df498d041cf1def987b4edf85a9da961
kamal <kamal.berriga@gmail.com>
2017-11-14 02:05:17 +0000

    new method to load facebook user

--- src/app/services/facebook-custom.service.ts
+++ src/app/services/facebook-custom.service.ts
@@ -1,46 +1,51 @@
 import { Injectable } from '@angular/core';
-import {FacebookService, InitParams, LoginResponse} from "ngx-facebook";
+import {FacebookService, InitParams, LoginOptions, LoginResponse} from "ngx-facebook";
 import {FacebookUser} from "../model/mode.FacebookUser";
 
 @Injectable()
 export class FacebookCustomService {
   public user: FacebookUser =new FacebookUser();
-  constructor(private fb: FacebookService, ) {
+  public token:string="";
+  constructor(private fb: FacebookService) {
 
     // this will init the app using the facebook app id
     let initParams: InitParams = {
-      appId: '1900664226929744',
+      appId: '127062021306633',
+     // status: true ,
       xfbml: true,
-      version: 'v2.8'
+      version: 'v2.11',
     };
     this.fb.init(initParams);
   }
 login(){
-  this.fb.login()
- .then((response: LoginResponse) =>{
-   //getting some user info , the user_id is the most importent
-  this.fb.api('/me?fields=name,id,email,picture,first_name,last_name').then((response) => {
-    this.user.id = response.id;
-    this.user.fullName = response.firstName+ " "+response.lastName;
-    this.user.photoUrl = "https://graph.facebook.com/" + response.id + "/picture?type=normal";
-    localStorage.setItem("currentUserFB",JSON.stringify(this.user));
-  }).catch((error: any) => console.error(error));
+ return  this.fb.login({
+   scope: 'user_photos',
+ }).then((response: LoginResponse) =>{
+    this.token=response.authResponse.accessToken;
+    this.loadUser();
   }).catch((error: any) => console.error(error));
 
 }
+//this is a private method invoked when user logged with facebook
+private loadUser(){
+  //getting some user info , the user_id is the most importent
+  return this.fb.api('/me?fields=name,id,email,picture,first_name,last_name&access_token='+this.token).then((response) => {
+    this.user.id = response.id;
+    this.user.fullName = response.firstName+ " "+response.lastName;
+    localStorage.setItem("currentUserFB",JSON.stringify(this.user));
+  }).catch((error: any) => console.error(error));
+}
 
-
-
-// load all user albums
+// load all user's albums
   getUserAlbums(){
-  console.log('/'+this.user.id+'/albums');
-    return this.fb.api('/'+this.user.id+'/albums?fields=created_time,cover_photo,name');
+  // retrieving the facebook user id
+    let user_id=JSON.parse(localStorage.getItem("currentUserFB")).id;
+    return this.fb.api('/'+user_id+'/albums?fields=created_time,cover_photo,name&access_token='+this.token);
   }
 
   // getting the album's cover photo picture
   getLambumsCoverPhotoPicture(coverPhotoID:string){
-    console.log(coverPhotoID);
-    return this.fb.api('/'+coverPhotoID+'?fields=picture');
+    return this.fb.api('/'+coverPhotoID+'/picture?type=normal&access_token='+this.token);
   }
 
 logOut() {
