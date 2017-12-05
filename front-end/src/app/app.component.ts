5d3909b7a0e64ade3f10fe1398ea0911613a08b4
kamal <kamal.berriga@gmail.com>
2017-12-05 21:20:15 +0000

    html changes

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'facebook application';
+  title = 'spring boot and angular 4 secure authentication';
   static API_URL="http://localhost:8080";
 }
